60a862a4af133ee422acd069405f507d027373b8
Raptor-200 <2638094732@qq.com>
2025-04-03 15:12:19 +0800

    避坑里两个小改动

--- packages/play/.storybook/preview.js
+++ packages/play/.storybook/preview.js
@@ -1,10 +1,12 @@
+import "raptor-element/dist/index.css";
+
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
